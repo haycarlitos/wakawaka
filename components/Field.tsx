7cd52c1df2e64f7df72f1bd74e2a3a0fd4e39d40
haycarlitos <carlos.castilloperera@gmail.com>
2022-10-21 21:59:35 -0500

    Add fieldplayer component

--- components/Field.tsx
+++ components/Field.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
+import field2 from '../assets/images/field2.jpg'
+import FieldPlayer from './FieldPlayer'
+
+  
+
+const players: { [key: string]: null[]} = {
+  FWD: [null, null, null],
+  MID: [null, null, null],
+  DEF: [null, null, null, null],
+  GKC: [null],
+}
+
+
 
 const Field = () => {
+
+ 
   return (
-    <View>
-      <Text>i</Text>
-    </View>
+    <ImageBackground 
+    source={field2}
+    resizeMode='cover'
+    style={{
+    width:'100%', 
+    aspectRatio: 2 / 3,
+    justifyContent:'space-around',
+    alignItems:'center'}}
+    >
+      {Object.keys(players).map((position) => (
+                <View style={{
+                  flexDirection:'row',
+                  justifyContent:'space-around',
+                  alignItems:'center',
+                  width:'100%'
+                }}
+                >
+                  {players[position].map((player) => (
+                    <FieldPlayer player={player} position={position} />
+                  ))}
+                </View>
+
+
+      ))}
+    </ImageBackground>
   )
 }
 
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,61 +1,14 @@
-import { StyleSheet, ImageBackground, SafeAreaView, Text, View} from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-//import { Text, View } from '../components/Themed';
-import field2 from '../assets/images/field2.jpg'
-import { FontAwesome5 } from '@expo/vector-icons';
+import React from 'react'
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Field from '../components/Field';
 
 
-const players: { [key: string]: null[]} = {
-  FWD: [null, null, null],
-  MID: [null, null, null],
-  DEF: [null, null, null, null],
-  GKC: [null],
-}
+
 
 export default function TabOneScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground 
-      source={field2}
-      resizeMode='cover'
-      style={{
-      width:'100%', 
-      aspectRatio: 2 / 3,
-      justifyContent:'space-around',
-      alignItems:'center'}}
-      >
-        {Object.keys(players).map((position) => (
-          <View style={{
-            flexDirection:'row',
-            justifyContent:'space-around',
-            alignItems:'center',
-            width:'100%'
-          }}
-          >
-            {players[position].map((player) => (
-              <View style={{
-                alignItems:'center',
-                width:'100%'}}
-              >
-                <FontAwesome5 
-                  name="tshirt"
-                  size={35} 
-                  color={player ? '#d170db' : '#5c5c5cbb'} 
-                /> 
-                <Text style={{
-                  backgroundColor: '#666666bb',
-                  color: 'white',
-                  fontSize: 12,
-                  padding:2,
-                  paddingHorizontal:7,
-                  fontWeight: 'bold',
-                  }}>{position}</Text>
-              </View>
-            ))}
-          </View>
-        ))}
-      </ImageBackground>
+      <Field />
     </SafeAreaView>
   );
 }
